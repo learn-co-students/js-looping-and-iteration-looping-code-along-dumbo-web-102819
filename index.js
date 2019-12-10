5f467644b63d0a34c8e4744094392e6159b12665
Imade Osifo <toxsik88@gmail.com>
2019-12-10 15:45:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+// Code your solutions in this 
+
+
+function writeCards(message, event)
+{
+    for (let i = 0; i < message.length; i++)
+    {
+        console.log(`Thank you, ${message[i]}, for the wonderful ${event} gift!`);
+    }
+}
